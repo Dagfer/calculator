ca050c6ac149242574936ba74f43dc9c7708cccc
Dagfer <137955962+Dagfer@users.noreply.github.com>
2024-03-18 18:38:39 -0400

    Add division and multiplication; html skeleton
    Added aforementioned functions and set up basic
    html skeleton

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function addMe(firstNumber, secondNumber){
-return firstNumber + secondNumber
+    return firstNumber + secondNumber
 };
 
 function substractMe(firstNumber, secondNumber){
     return firstNumber - secondNumber
+}
+
+function divideMe(firstNumber, secondNumber){
+    return firstNumber / secondNumber
+}
+
+function multiplyMe(firstNumber, secondNumber){
+    return firstNumber * secondNumber
 }
