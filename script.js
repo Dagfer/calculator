c0315838dcfed6df46ae03e0e76e3ba2453ef826
Dagfer <137955962+Dagfer@users.noreply.github.com>
2024-03-21 19:09:44 -0400

    Create variables and function for basic operation
    wrote variables for each of the elements in a
    basic operation and a function that deploys them

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function divideMe(firstNumber, secondNumber){
 function multiplyMe(firstNumber, secondNumber){
     return firstNumber * secondNumber
 }
+
+let firstNumber
+let secondNumber
+let operator = '' 
+
+function operate (firstNumber, operator, secondNumber){
+    if (operator === "+"){
+        return addMe(firstNumber, secondNumber)
+    }
+    else if (operator === "-"){
+        return substractMe(firstNumber, secondNumber)
+    }
+    else if (operator === '/'){
+        return divideMeMe(firstNumber, secondNumber)
+    }
+    else if (operator === '*' || operator === 'x'){
+        return multiplyMe(firstNumber, secondNumber)
+    }
+}
